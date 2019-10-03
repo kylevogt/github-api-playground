317f2ecf813e6039a1ae1aea1c61612be5b431c9
Kyle Vogt <usernameNA@live.com>
2019-10-03 16:54:32 -0500

    Add pagination support for getting pull requests

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,4 +1,6 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import * as parseLinkHeader from 'parse-link-header';
+import { Links } from 'parse-link-header';
 import { Service } from 'typedi';
 import { HttpService } from './../http/http.service';
 import { PullRequest } from './models/pull-request.model';
@@ -28,17 +30,33 @@ export class GithubService {
     }
 
     public async getRepositoryPullRequests(owner: string, repositoryName: string): Promise<PullRequest[]> {
-        const url: string = `/repos/${owner}/${repositoryName}/pulls`;
+        const url: string = `/repos/${owner}/${repositoryName}/pulls?state=all`;
         const config: AxiosRequestConfig = this.getBaseConfig();
 
+        return await this.requestPullRequests(url, config);
+    }
+
+    private async requestPullRequests(url: string, config: AxiosRequestConfig): Promise<PullRequest[]> {
         try {
+            console.log('Requesting pull requests', url);
             const response: AxiosResponse<PullRequest[]> = await this.http.get(url, config);
 
-            return response.data;
+            const links: Links | null = parseLinkHeader(response.headers.link);
+
+            if (links && links.next) {
+                console.log('There is another page', links.next.url);
+                return [
+                    ...response.data,
+                    ...await this.requestPullRequests(links.next.url, config),
+                ];
+            } else {
+                return response.data;
+            }
+
         } catch (error) {
             console.error(
                 'An error occured while requesting repository pull requests',
-                { owner, repositoryName, error },
+                { url, error },
             );
 
             throw error;
