ae7429b2f639c3cd25d1013d9e9be9b538d7f4be
Kyle Vogt <usernameNA@live.com>
2019-10-03 17:01:16 -0500

    Extended functionality to support pages on repo call too

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -19,9 +19,7 @@ export class GithubService {
         const config: AxiosRequestConfig = this.getBaseConfig();
 
         try {
-            const response: AxiosResponse<Repository[]> = await this.http.get(url, config);
-
-            return response.data;
+            return await this.requestAllPages<Repository>(url, config);
         } catch (error) {
             console.error('An error occured while requesting organization repositories', { organization, error });
 
@@ -33,26 +31,8 @@ export class GithubService {
         const url: string = `/repos/${owner}/${repositoryName}/pulls?state=all`;
         const config: AxiosRequestConfig = this.getBaseConfig();
 
-        return await this.requestPullRequests(url, config);
-    }
-
-    private async requestPullRequests(url: string, config: AxiosRequestConfig): Promise<PullRequest[]> {
         try {
-            console.log('Requesting pull requests', url);
-            const response: AxiosResponse<PullRequest[]> = await this.http.get(url, config);
-
-            const links: Links | null = parseLinkHeader(response.headers.link);
-
-            if (links && links.next) {
-                console.log('There is another page', links.next.url);
-                return [
-                    ...response.data,
-                    ...await this.requestPullRequests(links.next.url, config),
-                ];
-            } else {
-                return response.data;
-            }
-
+            return await this.requestAllPages<PullRequest>(url, config);
         } catch (error) {
             console.error(
                 'An error occured while requesting repository pull requests',
@@ -60,6 +40,23 @@ export class GithubService {
             );
 
             throw error;
+        }
+    }
+
+    private async requestAllPages<T>(url: string, config: AxiosRequestConfig): Promise<T[]> {
+        console.log('Requesting page...', url);
+        const response: AxiosResponse<T[]> = await this.http.get(url, config);
+
+        const links: Links | null = parseLinkHeader(response.headers.link);
+
+        if (links && links.next) {
+            console.log('There is another page', links.next.url);
+            return [
+                ...response.data,
+                ...await this.requestAllPages<T>(links.next.url, config),
+            ];
+        } else {
+            return response.data;
         }
     }
 
