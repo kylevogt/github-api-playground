38ba6362b95f003990b9915c82961289cf9ae485
Kyle Vogt <usernameNA@live.com>
2019-10-03 17:22:56 -0500

    Unit testing of pagination traversing

--- src/github/github.service.spec.ts
+++ src/github/github.service.spec.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from 'axios';
+import { expect } from 'chai';
 import { IMock, It, Mock, Times } from 'typemoq';
 import { HttpService } from './../http/http.service';
 import { GithubService } from './github.service';
@@ -32,7 +33,7 @@ describe('Github', () => {
         it('Specifies correct baseurl', async () => {
             httpMock
                 .setup((http) => http.get(It.isAny(), It.is((c) => c.baseURL === 'https://api.github.com')))
-                .returns(() => Promise.resolve({ data: {}, headers: {}} as AxiosResponse))
+                .returns(() => Promise.resolve({ data: [], headers: {}} as AxiosResponse))
                 .verifiable(Times.once());
 
             await service.getOrganizationRepositories('org-name');
@@ -46,12 +47,57 @@ describe('Github', () => {
                     It.isAny(),
                     It.is((c) => c.headers.Accept === 'application/vnd.github.v3+json'),
                 ))
-                .returns(() => Promise.resolve({ data: {}, headers: {}} as AxiosResponse))
+                .returns(() => Promise.resolve({ data: [], headers: {}} as AxiosResponse))
                 .verifiable(Times.once());
 
             await service.getOrganizationRepositories('org-name');
 
             httpMock.verifyAll();
+        });
+
+        it('Requests all pages', async () => {
+            httpMock
+                .setup((http) => http.get(
+                    '/orgs/org-name/repos',
+                    It.isAny(),
+                ))
+                .returns(() => Promise.resolve({
+                    data: [{}, {}],
+                    headers: {
+                        link: '<https://api.github.com/orgs/24790523/repos?page=2>; rel="next"',
+                    },
+                } as AxiosResponse))
+                .verifiable(Times.once());
+
+            httpMock
+                .setup((http) => http.get(
+                    'https://api.github.com/orgs/24790523/repos?page=2',
+                    It.isAny(),
+                ))
+                .returns(() => Promise.resolve({
+                    data: [{}, {}],
+                    headers: {
+                        link: '<https://api.github.com/orgs/24790523/repos?page=3>; rel="next"',
+                    },
+                } as AxiosResponse))
+                .verifiable(Times.once());
+
+            httpMock
+                .setup((http) => http.get(
+                    'https://api.github.com/orgs/24790523/repos?page=3',
+                    It.isAny(),
+                ))
+                .returns(() => Promise.resolve({
+                    data: [{}],
+                    headers: { },
+                } as AxiosResponse))
+                .verifiable(Times.once());
+
+            const result = await service.getOrganizationRepositories('org-name');
+
+            httpMock.verifyAll();
+            httpMock.verify((http) => http.get(It.isAny(), It.isAny()), Times.exactly(3));
+            expect(result.length).to.equal(5);
         });
     });
 
@@ -75,7 +121,7 @@ describe('Github', () => {
         it('Specifies correct baseurl', async () => {
             httpMock
                 .setup((http) => http.get(It.isAny(), It.is((c) => c.baseURL === 'https://api.github.com')))
-                .returns(() => Promise.resolve({ data: {}, headers: {}} as AxiosResponse))
+                .returns(() => Promise.resolve({ data: [], headers: {}} as AxiosResponse))
                 .verifiable(Times.once());
 
             await service.getRepositoryPullRequests('org-name', 'repo-name');
@@ -89,12 +135,57 @@ describe('Github', () => {
                     It.isAny(),
                     It.is((c) => c.headers.Accept === 'application/vnd.github.v3+json'),
                 ))
-                .returns(() => Promise.resolve({ data: {}, headers: {}} as AxiosResponse))
+                .returns(() => Promise.resolve({ data: [], headers: {}} as AxiosResponse))
                 .verifiable(Times.once());
 
             await service.getRepositoryPullRequests('org-name', 'repo-name');
 
             httpMock.verifyAll();
+        });
+
+        it('Requests all pages', async () => {
+            httpMock
+                .setup((http) => http.get(
+                    '/repos/org-name/repo-name/pulls?state=all',
+                    It.isAny(),
+                ))
+                .returns(() => Promise.resolve({
+                    data: [{}],
+                    headers: {
+                        link: '<https://api.github.com/repositories/24790523/pulls?state=all&page=2>; rel="next"',
+                    },
+                } as AxiosResponse))
+                .verifiable(Times.once());
+
+            httpMock
+                .setup((http) => http.get(
+                    'https://api.github.com/repositories/24790523/pulls?state=all&page=2',
+                    It.isAny(),
+                ))
+                .returns(() => Promise.resolve({
+                    data: [{}],
+                    headers: {
+                        link: '<https://api.github.com/repositories/24790523/pulls?state=all&page=3>; rel="next"',
+                    },
+                } as AxiosResponse))
+                .verifiable(Times.once());
+
+            httpMock
+                .setup((http) => http.get(
+                    'https://api.github.com/repositories/24790523/pulls?state=all&page=3',
+                    It.isAny(),
+                ))
+                .returns(() => Promise.resolve({
+                    data: [{}],
+                    headers: { },
+                } as AxiosResponse))
+                .verifiable(Times.once());
+
+            const result = await service.getRepositoryPullRequests('org-name', 'repo-name');
+
+            httpMock.verifyAll();
+            httpMock.verify((http) => http.get(It.isAny(), It.isAny()), Times.exactly(3));
+            expect(result.length).to.equal(3);
         });
     });
 });
